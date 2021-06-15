7d80e1e7cf0e7d306e6c5bc9c6765a710f3f6284
Jardel Urban <jardelurban3@gmail.com>
2021-06-15 16:29:58 -0300

    segunda aula  - finalizado front-end buscando dados da api

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -1,8 +1,33 @@
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TaskFieldMask";
+import {
+  Button,
+  Typography,
+  Container,
+  CircularProgress,
+} from "@material-ui/core";
+import { FormElementContainer } from "@styles/index.style";
+import useIndex from "data/hooks/pages/useIndex.page";
+import {
+  ProfissionaisPaper,
+  ProfissionaisContainer,
+} from "@styles/index.style";
 
 export default function Home() {
+  const {
+    cep,
+    setCep,
+    cepValido,
+    buscarProfissionais,
+    erro,
+    diaristas,
+    buscaFeita,
+    carregando,
+    diaristasRestantes,
+  } = useIndex();
+
   return (
     <div>
       <SafeEnvironment />
@@ -12,12 +37,68 @@ export default function Home() {
           "Preencha seu endereço e veja todos os profissionais da usa localidade"
         }
       />
-      <UserInformation
-        name={"Jardel Urban"}
-        picture={"https://github.com/j-rdel.png"}
-        rating={3}
-        description={"Florianópolis"}
-      />
+      <Container>
+        <FormElementContainer>
+          <TextFieldMask
+            variant={"outlined"}
+            mask={"99.999.999"}
+            label={"Digite seu CEP"}
+            fullWidth
+            value={cep}
+            onChange={(event) => setCep(event.target.value)}
+          />
+          {erro && <Typography color={"error"}>{erro}</Typography>}
+          <Button
+            sx={{ width: "220px" }}
+            variant={"contained"}
+            color={"secondary"}
+            disabled={!cepValido || carregando}
+            onClick={() => buscarProfissionais(cep)}
+          >
+            {carregando ? <CircularProgress size={"20"} /> : "Buscar"}
+          </Button>
+        </FormElementContainer>
+        {buscaFeita &&
+          (diaristas.length > 0 ? (
+            <ProfissionaisPaper>
+              <ProfissionaisContainer>
+                {diaristas.map((item, index) => {
+                  return (
+                    <UserInformation
+                      key={index}
+                      name={item.nome_completo}
+                      picture={item.foto_usuario}
+                      rating={item.reputacao}
+                      description={item.cidade}
+                    />
+                  );
+                })}
+              </ProfissionaisContainer>
+              <Container sx={{ textAlign: "center" }}>
+                {diaristasRestantes > 0 && (
+                  <Typography sx={{ mt: 5 }}>
+                    ...e mais {diaristasRestantes}{" "}
+                    {diaristasRestantes > 1
+                      ? "profissionais atendem"
+                      : "profissional atende"}{" "}
+                    ao seu endereço.
+                  </Typography>
+                )}
+                <Button
+                  variant={"contained"}
+                  color={"secondary"}
+                  sx={{ mt: 5 }}
+                >
+                  Contratar um profissional
+                </Button>
+              </Container>
+            </ProfissionaisPaper>
+          ) : (
+            <Typography align={"center"} color={"textPrimary"}>
+              Ainda não temos nenhuma diarista disponível em sua região
+            </Typography>
+          ))}
+      </Container>
     </div>
   );
 }
